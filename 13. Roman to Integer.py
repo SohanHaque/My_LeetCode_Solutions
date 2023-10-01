14fc7d8769a80f1df9fccad42143b768876db16f
Md Sohanul Haque <111044439+SohanHaque@users.noreply.github.com>
2023-10-01 19:01:26 +0600

    Add files via upload

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        for char in s[::-1]:
+            current_value = roman_to_int[char]
+            
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+                
+            prev_value = current_value
+        
+        return total
